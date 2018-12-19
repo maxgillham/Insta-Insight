1b67eeb8dd4b856d3fd23be033756e731d9763ab
ericbackman <eric.backman@sympatico.ca>
2018-12-19 18:52:53 -0500

    returns description for first 10-12 photos

--- utils.py
+++ utils.py
@@ -1,24 +1,21 @@
 from selenium import webdriver
 
+
 class InstagramScraper(object):
 
-    def __init__(self):
-        self.driver = webdriver.Chrome('./chromedriver/chromedriver')
-
-    def navigate_to(self, url):
-        self.driver.get(url)
-
     def get_account_photos(self, username):
+        driver = webdriver.Chrome('./chromedriver/chromedriver')
         url = "https://www.instagram.com/{}".format(username)
-        self.navigate_to(url)
-        photos = self.driver.find_elements_by_class_name("KL4Bh")
+        driver.get(url)
+        photos = driver.find_elements_by_class_name("KL4Bh")
         image_text = []
         for photo in photos:
             try:
                 image = photo.find_element_by_tag_name("img")
-                image_text.append(image)
+                image_text.append(image.get_attribute("alt"))
             except:
                 print("Failed")
+        driver.close()
         return image_text
 
 if __name__ == '__main__':
