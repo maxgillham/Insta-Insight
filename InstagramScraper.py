26b700f759e01ea1b1afb005db655f2609827b0a
ericbackman <eric.backman@sympatico.ca>
2018-12-19 18:43:45 -0500

    grabs image text for the first 12 or 13 photos

--- InstagramScraper.py
+++ InstagramScraper.py
@@ -1,14 +1,19 @@
 from selenium import webdriver
+import time
 
 class InstagramScraper(object):
 
-    def __init__(self):
+    def start_up(self):
         self.driver = webdriver.Chrome('chromedriver/chromedriver')
 
     def navigate_to(self, url):
         self.driver.get(url)
 
+    def shut_down(self):
+        self.driver.close()
+
     def get_account_photos(self, username):
+        self.start_up()
         url = "https://www.instagram.com/{}".format(username)
         self.navigate_to(url)
         photos = self.driver.find_elements_by_class_name("KL4Bh")
@@ -16,8 +21,9 @@ class InstagramScraper(object):
         for photo in photos:
             try:
                 image = photo.find_element_by_tag_name("img")
-                image_text.append(image)
+                image_text.append(image.get_attribute("alt"))
             except:
                 print("Failed")
+        self.shut_down()
         return image_text
 
