882406849905896efb9db9f5f678b22180902edb
maxgillham <maxfgillham@gmail.com>
2018-12-18 23:15:54 -0500

    erics scrapper added

--- utils.py
+++ utils.py
@@ -1,3 +1,27 @@
-'''
-Data scraper to be built here using bs4 and selenium
-'''
+from selenium import webdriver
+
+class InstagramScraper(object):
+
+    def __init__(self):
+        self.driver = webdriver.Chrome('./chromedriver/chromedriver')
+
+    def navigate_to(self, url):
+        self.driver.get(url)
+
+    def get_account_photos(self, username):
+        url = "https://www.instagram.com/{}".format(username)
+        self.navigate_to(url)
+        photos = self.driver.find_elements_by_class_name("KL4Bh")
+        image_text = []
+        for photo in photos:
+            try:
+                image = photo.find_element_by_tag_name("img")
+                image_text.append(image)
+            except:
+                print("Failed")
+        return image_text
+
+if __name__ == '__main__':
+    scraper = InstagramScraper()
+    selenium_objects = scraper.get_account_photos('maxgillham')
+    print(selenium_objects)
