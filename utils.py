fd117ab34982443fe0b66353074f13d1681360d5
maxgillham <maxfgillham@gmail.com>
2019-01-10 13:43:56 -0500

    returns links to page where likes and comments can be found

--- utils.py
+++ utils.py
@@ -17,8 +17,14 @@ class InstagramScraper(object):
                 image_text.append(image.get_attribute("alt"))
             except:
                 print("Failed")
+
+        #open photos and return like count
+        open_photos = driver.find_elements_by_class_name("_bz0w")
+        links = []
+        for photo in open_photos:
+            links.append(photo.find_element_by_tag_name('a').get_attribute('href'))
         driver.close()
-        return image_text
+        return image_text, links
 
 #This method will parse the image contents description to disclude labels
 def parse_img_description(image_text):
@@ -40,8 +46,9 @@ photo_contents = {
 if __name__ == '__main__':
     try:
         scraper = InstagramScraper()
-        selenium_objects = scraper.get_account_photos(sys.argv[1])
-        print(selenium_objects, '\n\n\nof length', len(selenium_objects))
-        print('\n\n\nparsed as ', parse_img_description(selenium_objects[:12]))
+        contents, links = scraper.get_account_photos(sys.argv[1])
+        print(contents)
+        print('\n\n\nparsed as ', parse_img_description(contents[:12]))
+        print('\n\n\nWith the following links', links)
     except:
         print('Must pass valid username as arguement to script')
