016c4f9e3d0af7b597f72a47d3a88a0623904b15
maxgillham <maxfgillham@gmail.com>
2019-01-09 20:00:32 -0500

    prints out most common photo content for past 12 photos :

--- insight.py
+++ insight.py
@@ -12,6 +12,8 @@ import sys
 def common_photo_contents(contents):
     objts = []
     for i in contents: objts.extend(i.split(', '))
+    for i in range(len(objts)):
+        if objts[i] in photo_contents: objts[i] = photo_contents[objts[i]]
     counts = np.unique(objts, return_counts=True)
     return counts
 
@@ -21,6 +23,9 @@ if __name__ == '__main__':
         scraper = InstagramScraper()
         selenium_objects = scraper.get_account_photos(sys.argv[1])
         counts = common_photo_contents(parse_img_description(selenium_objects[:12]))
-        print(counts)
+        max_ind = np.argmax(counts[1])
+        print('Detected photo contents', counts[0])
+        print('\nNumber of occurences in first 12 posts', counts[1])
+        print('\nCommon content contains', counts[0][np.argmax(counts[1])])
     except:
         print('Must pass valid username as arguement to script')
