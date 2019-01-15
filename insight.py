899ea6c9ccc05e88c97162c76bf739f80cd7c93c
maxgillham <maxfgillham@gmail.com>
2019-01-15 18:12:31 -0500

    sorting photo contents by like count

--- insight.py
+++ insight.py
@@ -17,16 +17,21 @@ def common_photo_contents(contents):
     counts = np.unique(objts, return_counts=True)
     return counts
 
+#sort photo description in terms of likes obtained for each photo
+def content_sorted_by_likes(contents, likes):
+    return [contents for _, contents in sorted(zip(likes, contents), reverse=True)]
+
 if __name__ == '__main__':
     try:
         scraper = InstagramScraper(sys.argv[1])
         contents = scraper.get_photo_contents()
         content_counts = common_photo_contents(parse_img_description(contents[:12]))
-        print('Detected photo contents', content_counts[0])
-        #print('\nNumber of occurences in first 12 posts', content_counts[1])
-        print('\nCommon content contains', content_counts[0][np.argmax(content_counts[1])])
         like_count = scraper.get_like_count()
+        top_content = content_sorted_by_likes(contents, like_count)
+        print('Detected photo contents', content_counts[0])
+        print('\nCommon content contains', content_counts[0][np.argmax(content_counts[1])])
         print('\nLikes obtained by each post', like_count)
+        print('\nMost likes on photos containing', top_content[:3])
         scraper.exit()
     except:
         print('Must pass valid username as arguement to script')
